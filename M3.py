8ed5bfb56265be88768ae0969f5fe4b315461109
gfark <98791795+gfark@users.noreply.github.com>
2023-09-12 11:11:51 -0400

    Added functions to draw -- not running

--- M3.py
+++ M3.py
@@ -20,14 +20,16 @@ class drawSomething:
         """
         self.transition_matrix = transition_matrix
         self.edges = list(transition_matrix.keys())
+        
 
     def get_next_color(self, current_edge):
         """
         Decides the next line to draw based off the current line
+        Args:
+            current_edge 
         """
+        
         temp = np.random.choice (self.edges,p= [self.transition_matrix[current_edge][next_edge] for next_edge in self.edges])
-
-        print(temp)
 
         return temp
     
@@ -37,6 +39,8 @@ class drawSomething:
         Args:
             current_color (string): color name
         """
+
+        print("step 2")
         angle = ANGLES.get(current_color)
         return angle
 
@@ -46,16 +50,35 @@ class drawSomething:
         Put the color and angle combination together in order to draw
         """
 
+        drawing_order = []
+        while len(self.line_order) < line_number:
+
+            next_color = self.get_next_color(current_color)
+            current_angle = self.get_next_angle(current_color)
+            coordinate = [current_color, current_angle]
+            current_color = next_color
+            drawing_order.append(coordinate)
         
+        return drawing_order
 
-    def draw(self, next_edge):
 
-        self.edges.append(next_edge)
-        return self.edges 
-    
+    def draw(self, sequence):
+        print(sequence)
+
+        
+        
+        t = Turtle()
+
+        for coord in sequence:
+            t.color(coord[0])
+            t.right(coord[1])
+            t.forward(100)
+
+        
     
 def main():
 
+    print("start")
     drawing = drawSomething({  
         
         "red": {"A": 0.2, "B": 0.5, "C": .03},
@@ -63,6 +86,10 @@ def main():
 		"yellow": {"A": 0.3, "B": 0.4, "C": .03}
 
     })
+    
+    new_drawing = drawing.drawing_sequence(current_color="red", line_number=30)
+    drawing.draw(new_drawing)
+
 
 
 if "__name__" == "__main__":
