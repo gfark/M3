64b23b0445e54b8641097f8446f2b5d69025a26e
gfark <98791795+gfark@users.noreply.github.com>
2023-09-14 15:55:48 -0400

    Commented out and added docstrings

--- M3.py
+++ M3.py
@@ -3,17 +3,42 @@ from turtle import *
 import turtle
 from random import *
 
+"""
+Author: Greta Farkas
+Course: Computations Creativity- CSCI 3725
+Assignment: M3 Markov Distinction
+Date: 9/14/23
+Description: 
+The file draws a night sky with stars based of an inputed color,
+and number of lines, which results in the number stars drawn.
+It contains the class drawSomething, which takes in a transition matrix of colors,
+to predicts the next move. The colors have corresponding angles which 
+aid in creating different styles of stars.
+
+
+
+"""
+
 
 ANGLES = {
 
     "black": 45,
-    "blue": 150,
+    "yellow": 150,
     "orange" : 155,
     "pink": 152.5
     
 }
 
+"""
+Class drawSomething:
 
+Functions:
+__init__ , get_next_color, get_next_angle, drawing_sequence, draw
+
+Description: 
+Contains functions that draw and aid in drawing a night sky with stars
+based off a probabiltiy matrix
+"""
 class drawSomething:
 
     def __init__(self, transition_matrix):
@@ -30,7 +55,7 @@ class drawSomething:
         """
         Decides the next line to draw based off the current line
         Args:
-            current_edge 
+            current_edge (): takes in the current color being used from the transition matric
         """
         
         temp = np.random.choice (self.edges,p= [self.transition_matrix[current_edge][next_edge] for next_edge in self.edges])
@@ -39,7 +64,7 @@ class drawSomething:
     
     def get_next_angle(self, current_color="red"):
         """
-        Retrieves the next angle based off the color chosen
+        Retrieves angle corresponding to the color chosen
         Args:
             current_color (string): color name
         """
@@ -51,54 +76,100 @@ class drawSomething:
 
     def drawing_sequence(self, current_color="red", line_number=20):
         """
-        Put the color and angle combination together in order to draw
-        """
+        Puts the color and angle combination in a list and adds that list to the drawings
+        sequence order
+        Args:
+            current_color (string): decides the inital color in the transition matrix to start with
+            line_number (int): input # of lines to include in the drawing
 
+        """
+        #list to store the sequence predicted
         drawing_order = []
+
+
         while len(drawing_order) < line_number:
 
+            #retrieves the next color from the transition matrix
             next_color = self.get_next_color(current_color)
+
+            #retrieves the star corner angle based of the color
             current_angle = self.get_next_angle(current_color)
+
+            #creates the angle and color pairing 
             coordinate = [current_color, current_angle]
+
+            #sets the current colors as the next color
             current_color = next_color
+
+            #adds the angle color pairing to the drawing sequence list
             drawing_order.append(coordinate)
         
         return drawing_order
 
 
-    def draw(self, sequence):
-        
+    def draw(self, current_color="yellow", line_number=200):
+        """
+        Function that draws the stars in the night sky and initiates the drawing
+        Args:
+            current_color (string): inital color in the probability matrix to start at
+            line_number (int): number of lines to be included in the stars 
+        """
+
+        #retreives the list of moves for the turtle to make
+        sequence = self.drawing_sequence(current_color, line_number)
     
+        #sets up the popup screen
         t = Turtle()
         turtle.Screen().bgcolor("black")
         turtle.screensize(900, 700)
+        t.speed(10)
+        t.ht()
+        
+        #initalizes variables that tell the turtle when to turn left and right
         turn = 0
+        count = 0
         for coord in sequence:
             
+            #sets the turtle color
             t.color(coord[0])
             
+            #if statement with specific directions for when the tranisition matrix is black and is starting
+            #to creat a new star
             if coord[1] == 45:
+                
+                t.penup()
+                
+                #if turn is 0, the turtle turns right after two turns
                 if turn == 0:
                     t.right(coord[1])
-                    turn = 1
+                    count += 1
+                    if count == 2:
+                        turn = 1
+                        count = 0
+
+                #if the turn is 1, the turtle turns left after two turns     
                 else:
                     t.left(coord[1])
-                    turn = 0   
-                
-                if t.xcor() > 900 or t.xcor() < -900:
-                    x = randint(-900,900)
-                    y = randint(-900, 900)
+                    count += 1
+                    if count == 2:
+                        turn = 0
+                        count = 0
+                      
+                #makes sure the turtle stays within the bounds of the screen
+                if t.xcor() > 300 or t.xcor() < -300:
+                    x = randint(-300,300)
+                    y = randint(-300, 300)
+
                     t.setpos(x,y)
                 
-                if t.ycor() > 700 or t.ycor() < -700:
-                    x = randint(-700,700)
-                    y = randint(-700, 700)
+                if t.ycor() > 100 or t.ycor() < -100:
+                    x = randint(-100,100)
+                    y = randint(-100, 100)
                     t.setpos(x,y)
                     
-                
-
-            
-                t.forward(250)
+        
+                t.forward(300)
+                t.pendown()
 
             else:
                 t.right(coord[1]) 
@@ -115,15 +186,15 @@ def main():
 
     drawing = drawSomething({  
         
-        "black": {"black": 0.1, "blue": 0.4, "orange": 0.3, "pink": 0.2},
-		"blue": {"black": 0.1, "blue": 0.7, "orange": 0.1, "pink": 0.1},
-		"orange": {"black": 0.1, "blue": 0.3, "orange": 0.4, "pink": 0.2},
-        "pink": {"black": 0.1, "blue": 0.2, "orange": 0.2, "pink": 0.5}
+        "black": {"black": 0.0, "yellow": 0.3, "orange": 0.4, "pink": 0.3},
+		"yellow": {"black": 0.1, "yellow": 0.7, "orange": 0.0, "pink": 0.2},
+		"orange": {"black": 0.1, "yellow": 0.2, "orange": 0.7, "pink": 0.0},
+        "pink": {"black": 0.1, "yellow": 0.0, "orange": 0.2, "pink": 0.7}
 
     })
 
-    to_draw = drawing.drawing_sequence(current_color="blue", line_number=200)
-    drawing.draw(to_draw)
+
+    drawing.draw("yellow", 200)
     
     
 
